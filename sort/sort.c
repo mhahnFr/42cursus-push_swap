dd00b1244aa43e755cb898637e716c0bf32e0257
mhahn <mhahn@student.42heilbronn.de>
2021-12-08 22:45:05 +0100

    Added step printing

--- sort/sort.c
+++ sort/sort.c
@@ -24,25 +24,31 @@ void	sort(struct s_stack_heads *heads)
 	for (size_t i = 0; i < a_size; i++) {
 		if (i == 0) {
 			stack_push(&heads->a, &heads->b);
+			write(1, "pb\n", 3);
 		} else {
 			size_t b_size = stack_size(heads->b);
 			bool inserted = false;
 			for (size_t j = 0; j < b_size; j++) {
 				if (heads->a->content < heads->b->content) {
 					stack_push(&heads->a, &heads->b);
+					write(1, "pb\n", 3);
 					inserted = true;
 					break;
 				}
 				stack_rotate(&heads->b, false);
+				write(1, "rb\n", 3);
 			}
 			if (!inserted) {
 				stack_push(&heads->a, &heads->b);
+				write(1, "pb\n", 3);
 			}
 		}
 	}
 	a_size = stack_size(heads->b);
-	for (size_t i = 0; i < a_size; i++)
+	for (size_t i = 0; i < a_size; i++) {
 		stack_push(&heads->b, &heads->a);
+		write(1, "pa\n", 3);
+	}
 }
 
 bool	is_sorted(struct s_stack_heads *heads)
