a2461f46b3bc50e7d78decccd445a75ad4bcbf3a
mhahn <mhahn@student.42heilbronn.de>
2021-12-09 16:19:47 +0100

    Added rotations

--- stack/stack_rotate.c
+++ stack/stack_rotate.c
@@ -2,7 +2,11 @@
 
 void	stack_rotate(struct s_stack **this, bool reverse)
 {
-	this = NULL;
-	if (reverse)
+	if (this == NULL || stack_size(*this) <= 1)
 		return ;
+	if (reverse)
+		*this = (*this)->previous;
+	else
+		*this = (*this)->next;
+	stack_repair_indices(*this);
 }
