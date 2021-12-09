f97afcc9039a1f63d8bca304d34f9aa046418ae3
mhahn <mhahn@student.42heilbronn.de>
2021-12-09 17:02:09 +0100

    Corrected pushing

--- stack/stack_push.c
+++ stack/stack_push.c
@@ -4,25 +4,44 @@ void	stack_push(struct s_stack **this, struct s_stack **to)
 {
 	struct s_stack	*tmp;
 
-	if (this == NULL || to == NULL || (*this == NULL && *to == NULL))
+	if (this == NULL || to == NULL || *this == NULL)
 		return ;
-	tmp = (*this)->next;
-	if (*to != NULL)
-		stack_append(this, *to);
-	*to = *this;
-	*this = tmp;
+	(*this)->previous->next = (*this)->next;
+	(*this)->next->previous = (*this)->previous;
+	tmp = *this;
+	*this = (*this)->next;
+	if (*this == tmp)
+		*this = NULL;
 	stack_repair_indices(*this);
+
+	if (*to != NULL)
+	{
+		(*to)->previous->next = tmp;
+		tmp->previous = (*to)->previous;
+		tmp->next = *to;
+		(*to)->previous = tmp;
+	}
+	else
+	{
+		tmp->previous = tmp;
+		tmp->next = tmp;
+	}
+	*to = tmp;
 	stack_repair_indices(*to);
 }
 
 void	stack_repair_indices(struct s_stack *this)
 {
-	size_t	ctr;
+	struct s_stack	*tmp;
+	size_t			ctr;
 
 	ctr = 0;
+	tmp = this;
 	while (this != NULL)
 	{
 		this->index = ctr++;
 		this = this->next;
+		if (this == tmp)
+			break ;
 	}
 }
