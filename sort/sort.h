313ac2ce8267041e9d6d741f3c8cdb89e42bf8d4
mhahn <mhahn@student.42heilbronn.de>
2021-12-15 21:46:19 +0100

    Norm fixes

--- sort/sort.h
+++ sort/sort.h
@@ -1,30 +1,43 @@
 #ifndef SORT_H
 # define SORT_H
 
+# include <stddef.h>
 # include <stdbool.h>
 
 # include "push_swap.h"
 
+typedef enum e_operation	t_blub;
+
+typedef struct s_arraylist	t_arraylist;
+
 /*
  * Returns wether the stacks are correctly sorted.
  */
-bool	is_sorted(struct s_stack_heads *heads);
+bool						is_sorted(struct s_stack_heads *heads);
 
 /*
  * Sorts the given stacks.
  */
-void	sort(struct s_stack_heads *heads);
+void						sort(struct s_stack_heads *heads);
 
 /*
  * Returns the count of operations that would be done by the default algorithm.
  * Returns zero if no heads are given.
  */
-size_t	get_ops_def(struct s_stack_heads *heads);
+size_t						get_ops_def(struct s_stack_heads *heads);
 
 /*
  * Returns the count of operations that would need to be done if the topmost
  * element of stack B would be skipped.
  */
-size_t	get_ops_def_next(struct s_stack_heads *heads);
+size_t						get_ops_def_next(struct s_stack_heads *heads);
+
+enum e_operation			get_default_operation(
+								t_arraylist *past,
+								struct s_stack_heads *heads);
+
+enum e_operation			simulate_next_operation(
+								t_arraylist *past,
+								struct s_stack_heads *heads);
 
 #endif
